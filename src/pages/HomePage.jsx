dbe41e362f67127d4a9630a323d0d9b060124670
salma <salmagueddi5@gmail.com>
2025-04-22 00:25:16 +0100

    Commit des modifications home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -44,7 +44,7 @@ const HomePage = () => {
 						No products found 😢{" "}
 						<Link to={"/create"}>
 							<Text as='span' color='blue.500' _hover={{ textDecoration: "underline" }}>
-								Create a product
+								Create a product Afef :p
 							</Text>
 						</Link>
 					</Text>
