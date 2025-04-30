d42f2efe4ab8cf4f7df467af403bd842c3e07797
salma <salmagueddi5@gmail.com>
2025-04-30 20:41:20 +0100

    modifier navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
 					bgGradient={"linear(to-r, cyan.400, blue.500)"}
 					bgClip={"text"}
 				>
-					<Link to={"/"}>Go DevOps'y - salma Store🛒</Link>
+					<Link to={"/"}> salma Store🛒</Link>
 				</Text>
 
 
